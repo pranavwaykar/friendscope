aac83fd727471ad5547fcfd5455d1e46052e6ec3
Chan Meng <Chan.Meng@lincolnuni.ac.nz>
2024-11-05 00:03:07 +1300

    Update LottieAnimation.tsx

--- components/LottieAnimation.tsx
+++ components/LottieAnimation.tsx
@@ -1,6 +1,31 @@
+// 'use client'
+//
+// import React from 'react';
+// import Lottie from 'lottie-react';
+// import type { LottieComponentProps } from 'lottie-react';
+//
+// interface LottieAnimationProps extends Partial<LottieComponentProps> {
+//     path: string;
+//     className?: string;
+// }
+//
+// export const LottieAnimation = ({ path, className, ...props }: LottieAnimationProps) => {
+//     return (
+//         <div className={className}>
+//             <Lottie
+//                 path={path}
+//                 loop={true}
+//                 autoplay={true}
+//                 {...props}
+//             />
+//         </div>
+//     );
+// };
+
+
 'use client'
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Lottie from 'lottie-react';
 import type { LottieComponentProps } from 'lottie-react';
 
@@ -10,10 +35,24 @@ interface LottieAnimationProps extends Partial<LottieComponentProps> {
 }
 
 export const LottieAnimation = ({ path, className, ...props }: LottieAnimationProps) => {
+    const [animationData, setAnimationData] = useState<any>(null);
+
+    useEffect(() => {
+        // Dynamically import the animation data
+        fetch(path)
+            .then(response => response.json())
+            .then(data => setAnimationData(data))
+            .catch(error => console.error('Error loading animation:', error));
+    }, [path]);
+
+    if (!animationData) {
+        return null; // Or a loading state
+    }
+
     return (
         <div className={className}>
             <Lottie
-                path={path}
+                animationData={animationData}
                 loop={true}
                 autoplay={true}
                 {...props}
